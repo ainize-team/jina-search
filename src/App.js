b02049dcc00d42a7b9ef79d7e8ee7bedb02e8fff
msh1273 <mch1273@naver.com>
2021-07-12 18:30:52 +0900

    tes1

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './App.css';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import HomeContainer from "./container/HomeContainer";
-// 이것은 테스트 주석입니다.
+//이것은 pr테스트용 주석입니다.!
 function App() {
   return (
       <div className="App">
